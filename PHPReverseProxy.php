29fad60f43c6ae04139125c112c4f4c5bf13afc9
sameershinde07 <38528049+sameershinde07@users.noreply.github.com>
2023-12-04 20:11:08 +0530

    Update PHPReverseProxy.php

--- PHPReverseProxy.php
+++ PHPReverseProxy.php
@@ -1,1 +1,7 @@
+<?php
+/* This is a multiline PHP
+comment. None of this
+will be shown to the user. */
+echo "This message will be printed to the user.";
+?>
 curl_setopt($ch,CURLOPT_COOKIE,$this->cookie);
