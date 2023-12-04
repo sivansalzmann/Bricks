3820475759ee882692acda0f79ad3ed7e78dbfb1
sameershinde07 <38528049+sameershinde07@users.noreply.github.com>
2023-12-04 20:14:08 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+
+
+/*echo “This is the first line of code”;
+echo “This is the second line of code”*/
+/*echo “This is the first line of code”;
+echo “This is the second line of code”*/
+/*echo “This is the first line of code”;
+echo “This is the second line of code”*/
+
+echo "<div class=\"alert-box success\">Upload succesful: <a href='$destination'>here</a><a href=\"\" class=\"close\">&times;</a></div>";
+/*echo “This is the first line of code”;
+echo “This is the second line of code”*/
